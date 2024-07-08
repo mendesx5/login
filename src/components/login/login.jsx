f6750df28f748c356f48c2560a57396dc36cf3f4
mendesx5 <bielmendes2208@gmail.com>
2024-07-08 19:10:23 -0300

    Initial commit

--- src/components/login/login.jsx
+++ src/components/login/login.jsx
@@ -1,6 +1,16 @@
-import {FaUser, FaLock} from 'react-icons/fa'
+import {FaUser, FaLock} from 'react-icons/fa';
+
+import { UserState } from 'react';
+
+import "./login.css";
 
 const Login = () => {
+
+    const [username, SetUsername] = UserState("");
+    const [password, setPassword] = UserState("");
+
+    
+
     return (
         <div className="container">
             <form>
@@ -13,7 +23,22 @@ const Login = () => {
                     <input type="password" placeholder="Passoword"/>
                     <FaLock className='icon'/>
                 </div>
+
+                <div className="recall-forget">
+                    <label>
+                        <input type="checkbox" />
+                        Lembre de mim
+                    </label>
+                    <a href="">Esqueceu a senha?</a>
+                </div>
+
                 <button>Entrar</button>
+
+                <div className="singup-link">
+                    <p>
+                        Não tem uma conta? <a href="">Registrar</a>
+                    </p>
+                </div>
             </form>
         </div>
     )
